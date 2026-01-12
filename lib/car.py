5099440dd96465fc9f890c84b07f7ef832c6321c
Kimberly-K04 <kkwamboka04@gmail.com>
2026-01-12 20:41:23 +0300

    done

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,8 @@
+# car.py
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    # We inherit everything from Vehicle
+    # Override the go() method to make it specific to Car
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
